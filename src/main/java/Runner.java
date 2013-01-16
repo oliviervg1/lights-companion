35ec9d9fe304b566abf80b24a91a6fc6524bd48e
Olivier Van Goethem <ov109@ic.ac.uk>
2013-01-16 15:17:47 +0000

    This should work

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,5 +4,20 @@ public class Runner {
     public static void main(String[] args) {
         // create controller instance and start it up
         PowerController powerSocket = new PowerController();
+        
+        while(true) {
+            System.out.println("");
+        	System.out.println("Turn Relay ON/OFF?");
+  
+	        String event = System.console().readLine();
+
+	        if (event.equalsIgnoreCase("on")) {
+	        	powerSocket.turnOn();
+	        }
+
+	        if (event.equalsIgnoreCase("off")) {
+	        	powerSocket.turnOff();
+	        }
+        }
     }
 }
--- src/main/java/com/relayClient/raspi/PowerController.java
+++ src/main/java/com/relayClient/raspi/PowerController.java
@@ -2,87 +2,34 @@ package com.relayClient.raspi;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
-import com.pi4j.io.gpio.trigger.GpioToggleStateTrigger;
 
-public class PowerController
-{
-    // internal class members
+public class PowerController {
+    
     private GpioController gpio;
-    private GpioPinDigitalInput overrideSwitch;
     private GpioPinDigitalOutput powerController;
     
-    
     public PowerController() {
-    	// create GPIO controller
         gpio  = GpioFactory.getInstance();
         
-        // provision GPIO pins : 
-        //   GPIO PIN #0 == OVERRIDE SWITCH
         //   GPIO PIN #1 == POWER CONTROLLER
-        overrideSwitch = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, "OverrideSwitch", PinPullResistance.PULL_DOWN);
         powerController = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PowerController");
         
         // force power controller to OFF if the program is shutdown
         powerController.setShutdownOptions(true,PinState.LOW);
         
-        // create a gpio toggle trigger on the override switch input pin; 
-        // when the input is detected, toggle the power controller state
-        overrideSwitch.addTrigger(new GpioToggleStateTrigger(PinState.HIGH, powerController));
-        
-        // create a listener for the override switch
-        overrideSwitch.addListener(new OverrideSwitchListener());
-        
-        powerController.high();
-        
-        while(true) {
-            System.out.println("");
-        	System.out.println("Turn Relay ON/OFF?");
-  
-	        String event = System.console().readLine();
-	      	        
-	        if (event.equalsIgnoreCase("on")) {
-	        	powerController.high();
-	        }
-	        
-	        if (event.equalsIgnoreCase("off")) {
-	        	powerController.low();
-	        }
-        }
+        // default to off
+        powerController.low();
     }
   
-//    public void turnOn() {
-//    	powerController.high();
-//    }
-//    
-//    public void turnOff() {
-//    	powerController.low();
-//    }
+    public void turnOn() {
+    	powerController.high();
+    }
     
-    /**
-     * This listener class is invoked as a callback when a state change
-     * is detected on the override input switch (if implemented; optional)
-     * 
-     * @author Robert Savage
-     */
-    private class OverrideSwitchListener implements GpioPinListenerDigital
-    {
-        @Override
-        public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event)
-        {
-            if(event.getState().isHigh())
-            {
-                System.out.println("---------------------------------");
-                System.out.println("[OVERRIDE] POWER STATE TOGGLED");
-                System.out.println("---------------------------------");
-            }
-        }
-    }  
+    public void turnOff() {
+    	powerController.low();
+    }
 }
         
