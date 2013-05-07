b2da71040a0132111c7fedf789cdc23e2ff75eae
Olivier Van Goethem <ov109@ic.ac.uk>
2013-05-07 10:39:27 +0100

    Updated dependencies

--- src/main/java/PublishServer.java
+++ src/main/java/PublishServer.java
@@ -5,6 +5,6 @@ import com.pi.relay.SocketController;
 // DEPRECATED - REPLACED WITH DEPLOYABLE WAR FOR USE WITH TOMCAT
 public class PublishServer {
     public static void main(String[] args) {
-    	Endpoint.publish("http://localhost:8080/relay-control-1.0.0/SocketController", new SocketController());
+    	Endpoint.publish("http://localhost:8080/lights-companion-1.0.0/SocketController", new SocketController());
     }   	
 }
